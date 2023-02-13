d1219c407c4e99a94a5ee81ae6cb5312e8f542ed
Fernanda Acuna <fdafilgueira@gmail.com>
2023-02-13 10:45:55 -0300

    cambios en el formato

--- src/validator.js
+++ src/validator.js
@@ -1,62 +1,51 @@
 const validator = {
   isValid,
   maskify,
-}
+};
 
 function isValid(creditCardNumber) {
-  const darloVuelta = creditCardNumber.split("").reverse()
+  const darloVuelta = creditCardNumber.split("").reverse();
 
-  const almacenaNumeros = []
+  const almacenaNumeros = [];
   for (let i = 0; i < darloVuelta.length; i++) {
-
     if ((i + 1) % 2 === 0) {
-
       const numeroMultiplicado = darloVuelta[i] * 2;
 
       if (numeroMultiplicado >= 10) {
-
         const numeroSeparado = numeroMultiplicado.toString().split("");
 
         const primerNumero = parseInt(numeroSeparado[0]); // 1
         const segundoNumero = parseInt(numeroSeparado[1]); // 4
         const resultadoSuma = primerNumero + segundoNumero;
         almacenaNumeros.push(resultadoSuma);
-
-
-
       } else {
-
         almacenaNumeros.push(numeroMultiplicado);
       }
     } else {
-
-      almacenaNumeros.push(parseInt(darloVuelta[i]))
+      almacenaNumeros.push(parseInt(darloVuelta[i]));
     }
-
   }
 
   const sumaTotal = almacenaNumeros.reduce((anterior, actual) => {
     return anterior + actual;
-  })
+  });
 
   if (sumaTotal % 10 === 0) {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
-
 }
 function maskify(ocultarNumeros) {
   const n = ocultarNumeros.slice(-4);
-  
+
   let mascara = "";
-  
+
   for (let i = 0; i < ocultarNumeros.length - 4; i++) {
     mascara = mascara + "#";
-    
   }
   const totalGatitos = mascara + n;
-  
+
   return totalGatitos;
 }
 
